d67251c2f5c7516414bd2a171909114444583703
43Clock <a89600@alunos.uminho.pt>
2021-05-26 17:54:11 +0100

    Cenas

--- TP2/compilador_lex.py
+++ TP2/compilador_lex.py
@@ -1,23 +1,84 @@
 import ply.lex as lex
 import sys
 
-tokens = ['ID','INT','FLOAT','INT_A','FLOAT_A','STRING_A','EQUAL','STRING',
-            'AND','OR','LESS_EQUAL','MORE_EQUAL','EQUAL_EQUAL','PRINT']
-literals = ['+','-','*','/','(',')',';','{','}','[',']','>','<','!']
+tokens = ['ID','INT','FLOAT','INT_A','FLOAT_A','STRING_A','EQUAL','STRING','FOR','READ',
+            'AND','OR','LESS_EQUAL','MORE_EQUAL','EQUAL_EQUAL','DIF','PRINT','IF','ELSE']
+literals = ['+','-','*','/','%','(',')',';','{','}','[',']','>','<','!']
 
-t_AND = r'&&'
-t_OR = r'\|\|'
-t_LESS_EQUAL = r'\<\='
-t_MORE_EQUAL = r'\>\='
-t_EQUAL_EQUAL = r'\=\='
-t_PRINT = r'print\ +'
-t_INT_A = r'int\ +'
-t_FLOAT_A = r'float\ +'
-t_STRING_A = r'string\ +'
-t_ID = r'\w+\ *'
-t_EQUAL = r'=\ *'
-t_STRING = r'\"[^"]+\"'
+def t_READ(t):
+    r'read\ *'
+    t.value = str(t.value)
+    return t
 
+def t_FOR(t):
+    r'for\ *'
+    t.value = str(t.value)
+    return t
+
+def t_IF(t):
+    r'if\ *'
+    t.value = str(t.value)
+    return t
+
+def t_ELSE(t):
+    r'else\ *'
+    t.value = str(t.value)
+    return t
+
+def t_AND(t):
+    r'&&'
+    t.value = str(t.value)
+    return t
+
+def t_OR(t):
+    r'\|\|'
+    t.value = str(t.value)
+    return t
+
+def t_LESS_EQUAL(t):
+    r'\<\='
+    t.value = str(t.value)
+    return t
+
+def t_MORE_EQUAL(t):
+    r'\>\='
+    t.value = str(t.value)
+    return t
+
+def t_EQUAL_EQUAL(t):
+    r'\=\='
+    t.value = str(t.value)
+    return t
+
+def t_DIF(t):
+    r'\!\='
+    t.value = str(t.value)
+    return t
+
+def t_PRINT(t):
+    r'print\ +'
+    t.value = str(t.value)
+    return t
+
+def t_INT_A(t):
+    r'int\ +'
+    t.value = str(t.value)
+    return t
+
+def t_FLOAT_A(t):
+    r'float\ +'
+    t.value = str(t.value)
+    return t
+
+def t_STRING_A(t):
+    r'string\ +'
+    t.value = str(t.value)
+    return t
+
+def t_EQUAL(t):
+    r'=\ *'
+    t.value = str(t.value)
+    return t
 
 def t_FLOAT(t):
     r'\d+\.\d+'
@@ -29,6 +90,15 @@ def t_INT(t):
     t.value = int(t.value)
     return t
     
+def t_ID(t):
+    r'\w+\ *'
+    t.value = str(t.value)
+    return t
+
+def t_STRING(t):
+    r'\"[^"]+\"'
+    t.value = str(t.value)
+    return t    
 
 def t_error(t):
     print('Illegal character: '+t.value[0])
@@ -38,3 +108,8 @@ def t_error(t):
 t_ignore = ' \n\t'
 
 lexer = lex.lex()
+
+""" for line in sys.stdin:
+    lexer.input(line)
+    for tok in lexer:
+        print(tok) """
--- TP2/compilador_yacc.py
+++ TP2/compilador_yacc.py
@@ -1,9 +1,12 @@
-import ply.yacc as yacc
 import sys
+
+import ply.yacc as yacc
+import re
 
 output = open("output.vm","w+")
 
 from compilador_lex import tokens
+
 
 def p_body(p):
     "body : atribuicoes operacoes"
@@ -19,27 +22,17 @@ def p_atribuicoes_vazio(p):
 
 def p_atribuicao_int(p):
     "atribuicao : INT_A ID EQUAL INT ';'"
-    p.parser.variaveis_int[p[2].strip()] = p.parser.variaveis
-    p.parser.variaveis += 1
-    p[0] = f"pushi {p[4]}\n"
-
-def p_atribuicao_float(p):
-    "atribuicao : FLOAT_A ID EQUAL FLOAT ';'"
-    p.parser.variaveis_float[p[2].strip()] = p.parser.variaveis
-    p.parser.variaveis += 1
-    p[0] = f"pushf {p[4]}\n"
+    if p.parser.sucess:
+        if not p[2].strip() in p.parser.variaveis_int and not p[2].strip() in p.parser.variaveis_arrays and not p[2].strip() in p.parser.variaveis_arrays2d and not p[2].strip() in p.parser.variaveis_string:
+        p.parser.variaveis_int[p[2].strip()] = p.parser.variaveis
+        p.parser.variaveis += 1
+        p[0] = f"pushi {p[4]}\n"
 
 def p_atribuicao_int_null(p):
     "atribuicao : INT_A ID ';'"
     p.parser.variaveis_int[p[2].strip()] = p.parser.variaveis
     p.parser.variaveis += 1
     p[0] = "pushi 0\n"
-
-def p_atribuicao_float_null(p):
-    "atribuicao : FLOAT_A ID ';'"
-    p.parser.variaveis_float[p[2].strip()] = p.parser.variaveis
-    p.parser.variaveis += 1
-    p[0] = "pushf 0.0\n"
 
 def p_atribuicao_array(p):
     "atribuicao : INT_A ID '[' INT ']' ';'"
@@ -53,7 +46,6 @@ def p_atribuicao_array2d(p):
     p.parser.variaveis += p[4]*p[7]
     p[0] = f"pushn {p[4]*p[7]}\n"
 
-
 def p_atribuicao_string(p):
     "atribuicao : STRING_A ID EQUAL STRING ';'"
     p.parser.variaveis_string[p[2].strip()] = p.parser.variaveis
@@ -63,6 +55,10 @@ def p_atribuicao_string(p):
 
 def p_operacoes_operacao(p):
     "operacoes : operacoes operacao"
+    p[0] = p[1] + p[2]
+    
+def p_operacoes_operacao_inc(p):
+    "operacoes : operacoes operacao_inc"
     p[0] = p[1] + p[2]
 
 def p_operacoes_fim(p):
@@ -78,16 +74,183 @@ def p_operacao_variavel(p):
         aux += f"storeg {p.parser.variaveis_float[p[1].strip()]}\n"
     p[0] = aux
 
+def p_operacao_inc_variavel_incrementar(p):
+    "operacao_inc : ID '+' '+' ';'"
+    if p[1].strip() in p.parser.variaveis_int:
+        aux = f"pushg {p.parser.variaveis_int[p[1].strip()]}\n"
+        aux += f"pushi 1\n"
+        aux += f"add\n"
+        aux += f"storeg {p.parser.variaveis_int[p[1].strip()]}\n"
+    p[0] = aux
+
+def p_operacao_inc_variavel_decrementar(p):
+    "operacao_inc : ID '-' '-' ';'"
+    if p[1].strip() in p.parser.variaveis_int:
+        aux = f"pushg {p.parser.variaveis_int[p[1].strip()]}\n"
+        aux += f"pushi 1\n"
+        aux += f"sub\n"
+        aux += f"storeg {p.parser.variaveis_int[p[1].strip()]}\n"
+    p[0] = aux
+
+def p_operacao_inc_variavel_plus_equal(p):
+    "operacao_inc : ID '+' EQUAL expressao ';'"
+    if p[1].strip() in p.parser.variaveis_int:
+        aux = f"pushg {p.parser.variaveis_int[p[1].strip()]}\n"
+        aux += f"{p[4]}\n"
+        aux += f"add\n"
+        aux += f"storeg {p.parser.variaveis_int[p[1].strip()]}\n"
+    p[0] = aux
+
+def p_operacao_inc_variavel_minus_equal(p):
+    "operacao_inc : ID '-' EQUAL expressao ';'"
+    if p[1].strip() in p.parser.variaveis_int:
+        aux = f"pushg {p.parser.variaveis_int[p[1].strip()]}\n"
+        aux += f"{p[4]}\n"
+        aux += f"sub\n"
+        aux += f"storeg {p.parser.variaveis_int[p[1].strip()]}\n"
+    p[0] = aux
+
 
 #TODO verificar que n está outofbounds e que de facto é um array
 def p_operacao_array_valor(p):
     "operacao : ID '[' INT ']' EQUAL expressoes ';' "
     aux  = "pushgp\n"
-    aux += f"pushi {p.parser.variaveis_arrays[p[1]][0]}\n"
+    aux += f"pushi {p.parser.variaveis_arrays[p[1].strip()][0]}\n"
     aux += "padd\n"
     aux += f"pushi {p[3]}\n"
     aux += f"{p[6]}\n"
     aux += "storen\n"
+    p[0] = aux
+
+def p_operacao_array_valor_id(p):
+    "operacao : ID '[' ID ']' EQUAL expressoes ';' "
+    aux  = "pushgp\n"
+    aux += f"pushi {p.parser.variaveis_arrays[p[1].strip()][0]}\n"
+    aux += "padd\n"
+    aux += f"pushg {p.parser.variaveis_int[p[3].strip()]}\n"
+    aux += f"{p[6]}\n"
+    aux += "storen\n"
+    p[0] = aux
+
+def p_operacao_inc_array_incrementar(p):
+    "operacao_inc : ID '[' INT ']' '+' '+' ';'"
+    if p[1].strip() in p.parser.variaveis_arrays:
+        aux  = "pushgp\n"
+        aux += f"pushi {p.parser.variaveis_arrays[p[1].strip()][0]}\n"
+        aux += "padd\n"
+        aux += f"pushi {p[3]}\n"
+        aux += f"pushg {p.parser.variaveis_arrays[p[1].strip()][0]+p[3]}\n"
+        aux += f"pushi 1\n" 
+        aux += f"add\n"
+        aux += f"storen\n"
+    p[0] = aux
+    
+
+def p_operacao_inc_array_incrementar_id(p):
+    "operacao_inc : ID '[' ID ']' '+' '+' ';'"
+    if p[1].strip() in p.parser.variaveis_arrays and p[3].strip() in p.parser.variaveis_int:
+        aux  = "pushgp\n"
+        aux += f"pushi {p.parser.variaveis_arrays[p[1].strip()][0]}\n"
+        aux += "padd\n"
+        aux += f"pushg {p.parser.variaveis_int[p[3].strip()]}\n"
+        aux += "pushgp\n"
+        aux += f"pushi {p.parser.variaveis_arrays[p[1].strip()][0]}\n"
+        aux += "padd\n" #Fazer isto again para ir buscar o valor na posicao ID
+        aux += f"pushg {p.parser.variaveis_int[p[3].strip()]}\n"
+        aux += "loadn\n"
+        aux += f"pushi 1\n" 
+        aux += f"add\n"
+        aux += f"storen\n"
+    p[0] = aux
+
+def p_operacao_inc_array_decrementar(p):
+    "operacao_inc : ID '[' INT ']' '-' '-' ';'"
+    if p[1].strip() in p.parser.variaveis_arrays:
+        aux  = "pushgp\n"
+        aux += f"pushi {p.parser.variaveis_arrays[p[1].strip()][0]}\n"
+        aux += "padd\n"
+        aux += f"pushi {p[3]}\n"
+        aux += f"pushg {p.parser.variaveis_arrays[p[1].strip()][0]+p[3]}\n"
+        aux += f"pushi 1\n" 
+        aux += f"sub\n"
+        aux += f"storen\n"
+    p[0] = aux
+
+def p_operacao_inc_array_decrementar_id(p):
+    "operacao_inc : ID '[' ID ']' '-' '-' ';'"
+    if p[1].strip() in p.parser.variaveis_arrays and p[3].strip() in p.parser.variaveis_int:
+        aux  = "pushgp\n"
+        aux += f"pushi {p.parser.variaveis_arrays[p[1].strip()][0]}\n"
+        aux += "padd\n"
+        aux += f"pushg {p.parser.variaveis_int[p[3].strip()]}\n"
+        aux += "pushgp\n"
+        aux += f"pushi {p.parser.variaveis_arrays[p[1].strip()][0]}\n"
+        aux += "padd\n" #Fazer isto again para ir buscar o valor na posicao ID
+        aux += f"pushg {p.parser.variaveis_int[p[3].strip()]}\n"
+        aux += "loadn\n"
+        aux += f"pushi 1\n" 
+        aux += f"sub\n"
+        aux += f"storen\n"
+    p[0] = aux
+
+def p_operacao_inc_array_plus_equal(p):
+    "operacao_inc : ID '[' INT ']' '+' EQUAL expressao ';'"
+    if p[1].strip() in p.parser.variaveis_arrays:
+        aux  = "pushgp\n"
+        aux += f"pushi {p.parser.variaveis_arrays[p[1].strip()][0]}\n"
+        aux += "padd\n"
+        aux += f"pushi {p[3]}\n"
+        aux += f"pushg {p.parser.variaveis_arrays[p[1].strip()][0]+p[3]}\n"
+        aux += f"{p[7]}\n"
+        aux += f"add\n"
+        aux += f"storen\n"
+    p[0] = aux
+
+def p_operacao_inc_array_plus_equal_id(p):
+    "operacao_inc : ID '[' ID ']' '+' EQUAL expressao ';'"
+    if p[1].strip() in p.parser.variaveis_arrays and p[3].strip() in p.parser.variaveis_int:
+        aux  = "pushgp\n"
+        aux += f"pushi {p.parser.variaveis_arrays[p[1].strip()][0]}\n"
+        aux += "padd\n"
+        aux += f"pushg {p.parser.variaveis_int[p[3].strip()]}\n"
+        aux += "pushgp\n"
+        aux += f"pushi {p.parser.variaveis_arrays[p[1].strip()][0]}\n"
+        aux += "padd\n" #Fazer isto again para ir buscar o valor na posicao ID
+        aux += f"pushg {p.parser.variaveis_int[p[3].strip()]}\n"
+        aux += "loadn\n"
+        aux += f"{p[7]}\n"
+        aux += f"add\n"
+        aux += f"storen\n"
+    p[0] = aux
+
+def p_operacao_inc_array_minus_equal(p):
+    "operacao_inc : ID '[' INT ']' '-' EQUAL expressao ';'"
+    if p[1].strip() in p.parser.variaveis_arrays:
+        aux  = "pushgp\n"
+        aux += f"pushi {p.parser.variaveis_arrays[p[1].strip()][0]}\n"
+        aux += "padd\n"
+        aux += f"pushi {p[3]}\n"
+        aux += f"pushg {p.parser.variaveis_arrays[p[1].strip()][0]+p[3]}\n"
+        aux += f"{p[7]}\n"
+        aux += f"sub\n"
+        aux += f"storen\n"
+    p[0] = aux
+
+def p_operacao_inc_array_minus_equal_id(p):
+    "operacao_inc : ID '[' ID ']' '-' EQUAL expressao ';'"
+    if p[1].strip() in p.parser.variaveis_arrays and p[3].strip() in p.parser.variaveis_int:
+        aux  = "pushgp\n"
+        aux += f"pushi {p.parser.variaveis_arrays[p[1].strip()][0]}\n"
+        aux += "padd\n"
+        aux += f"pushg {p.parser.variaveis_int[p[3].strip()]}\n"
+        aux += "pushgp\n"
+        aux += f"pushi {p.parser.variaveis_arrays[p[1].strip()][0]}\n"
+        aux += "padd\n" #Fazer isto again para ir buscar o valor na posicao ID
+        aux += f"pushg {p.parser.variaveis_int[p[3].strip()]}\n"
+        aux += "loadn\n"
+        aux += f"{p[7]}\n"
+        aux += f"sub\n"
+        aux += f"storen\n"
     p[0] = aux
 
 #TODO verificar que n está outofbounds e que de facto é um array
@@ -101,13 +264,111 @@ def p_operacao_array2d_valor(p):
     aux += "storen\n"
     p[0] = aux
 
+def p_operacao_inc_array2d_incrementar(p):
+    "operacao_inc : ID '[' INT ']' '[' INT ']' '+' '+' ';'"
+    if p[1].strip() in p.parser.variaveis_arrays2d:
+        aux  = "pushgp\n"
+        aux += f"pushi {p.parser.variaveis_arrays2d[p[1].strip()][0]}\n"
+        aux += "padd\n"
+        aux += f"pushi {p[3]*p.parser.variaveis_arrays2d[p[1].strip()][2]+p[6]}\n"
+        aux += f"pushg {p.parser.variaveis_arrays2d[p[1].strip()][0]+p[3]*p.parser.variaveis_arrays2d[p[1].strip()][2]+p[6]}\n"
+        aux += f"pushi 1\n" 
+        aux += f"add\n"
+        aux += f"storen\n"
+    p[0] = aux
+
+def p_operacao_inc_array2d_decrementar(p):
+    "operacao_inc : ID '[' INT ']' '[' INT ']' '-' '-' ';'"
+    if p[1].strip() in p.parser.variaveis_arrays2d:
+        aux  = "pushgp\n"
+        aux += f"pushi {p.parser.variaveis_arrays2d[p[1].strip()][0]}\n"
+        aux += "padd\n"
+        aux += f"pushi {p[3]*p.parser.variaveis_arrays2d[p[1].strip()][2]+p[6]}\n"
+        aux += f"pushg {p.parser.variaveis_arrays2d[p[1].strip()][0]+p[3]*p.parser.variaveis_arrays2d[p[1].strip()][2]+p[6]}\n"
+        aux += f"pushi 1\n" 
+        aux += f"sub\n"
+        aux += f"storen\n"
+    p[0] = aux
+
+def p_operacao_inc_array2d_plus_equal(p):
+    "operacao_inc : ID '[' INT ']' '[' INT ']' '+' EQUAL expressao ';'"
+    if p[1].strip() in p.parser.variaveis_arrays2d:
+        aux  = "pushgp\n"
+        aux += f"pushi {p.parser.variaveis_arrays2d[p[1].strip()][0]}\n"
+        aux += "padd\n"
+        aux += f"pushi {p[3]*p.parser.variaveis_arrays2d[p[1].strip()][2]+p[6]}\n"
+        aux += f"pushg {p.parser.variaveis_arrays2d[p[1].strip()][0]+p[3]*p.parser.variaveis_arrays2d[p[1].strip()][2]+p[6]}\n"
+        aux += f"{p[10]}\n"
+        aux += f"add\n"
+        aux += f"storen\n"
+    p[0] = aux
+
+def p_operacao_inc_array2d_minus_equal(p):  
+    "operacao_inc : ID '[' INT ']' '[' INT ']' '-' EQUAL expressao ';'"
+    if p[1].strip() in p.parser.variaveis_arrays2d:
+        aux  = "pushgp\n"
+        aux += f"pushi {p.parser.variaveis_arrays2d[p[1].strip()][0]}\n"
+        aux += "padd\n"
+        aux += f"pushi {p[3]*p.parser.variaveis_arrays2d[p[1].strip()][2]+p[6]}\n"
+        aux += f"pushg {p.parser.variaveis_arrays2d[p[1].strip()][0]+p[3]*p.parser.variaveis_arrays2d[p[1].strip()][2]+p[6]}\n"
+        aux += f"{p[10]}\n"
+        aux += f"sub\n"
+        aux += f"storen\n"
+    p[0] = aux
+
+def p_operacao_read_variavel(p):
+    "operacao : READ ID ';' "
+    aux = ""
+    if p[2].strip() in p.parser.variaveis_int:
+        aux += "read\n"
+        aux += "atoi\n"
+        aux += f"storeg {p.parser.variaveis_int[p[2].strip()]}\n"
+    p[0] = aux
+
+def p_operacao_read_array(p):
+    "operacao : READ ID '[' INT ']' ';'"
+    aux = ""
+    if p[2].strip() in p.parser.variaveis_arrays:
+        aux += "pushgp\n"
+        aux += f"pushi {p.parser.variaveis_arrays[p[2].strip()][0]}\n"
+        aux += "padd\n"
+        aux += f"pushi {p[4]}\n"
+        aux += f"read\n"
+        aux += f"atoi\n"
+        aux += "storen\n"
+    p[0] = aux
+
+def p_operacao_read_array_id(p):
+    "operacao : READ ID '[' ID ']' ';'"
+    aux = ""
+    if p[2].strip() in p.parser.variaveis_arrays:
+        aux += "pushgp\n"
+        aux += f"pushi {p.parser.variaveis_arrays[p[2].strip()][0]}\n"
+        aux += "padd\n"
+        aux += f"pushg {p.parser.variaveis_int[p[4].strip()]}\n"
+        aux += f"read\n"
+        aux += f"atoi\n"
+        aux += "storen\n"
+    p[0] = aux
+
+def p_operacao_read_array2d(p):
+    "operacao : READ ID '[' INT ']' '[' INT ']' ';' "
+    if p[2].strip() in p.parser.variaveis_arrays2d:
+        aux  = "pushgp\n"
+        aux += f"pushi {p.parser.variaveis_arrays2d[p[2].strip()][0]}\n"
+        aux += "padd\n"
+        aux += f"pushi {p[4]*p.parser.variaveis_arrays2d[p[2].strip()][2]+p[7]}\n"
+        aux += f"read\n"
+        aux += f"atoi\n"
+        aux += "storen\n"
+    p[0] = aux
 
 def p_expressoes_expressao(p):
     "expressoes : expressao"
     p[0] = p[1]
 
 def p_expressoes_expressao_logica(p):
-    "expressoes : expressao_logica"
+    "expressoes : expressao_logica" 
     p[0] = p[1]
 
 def p_expressao_mais(p):
@@ -130,6 +391,10 @@ def p_termo_div(p):
     "termo : termo '/' fator"
     p[0] = f"{p[1]}\n{p[3]}\ndiv\n"
 
+def p_termo_mod(p):
+    "termo : termo '%' fator"
+    p[0] = f"{p[1]}\n{p[3]}\nmod\n"
+
 def p_termo_fator(p):
     "termo : fator"
     p[0] = p[1]
@@ -145,10 +410,10 @@ def p_fator_expressao(p):
 
 def p_fator_int(p):
     "fator : ID"
-    if p[1] in p.parser.variaveis_int:
-        p[0] = f"pushg {p.parser.variaveis_int[p[1]]}"
-    elif p[1] in p.parser.variaveis_float:
-        p[0] = f"pushg {p.parser.variaveis_float[p[1]]}"
+    if p[1].strip() in p.parser.variaveis_int:
+        p[0] = f"pushg {p.parser.variaveis_int[p[1].strip()]}"
+    elif p[1].strip() in p.parser.variaveis_float:
+        p[0] = f"pushg {p.parser.variaveis_float[p[1].strip()]}"
 
 #TODO outofbounds
 def p_fator_array(p):
@@ -157,6 +422,15 @@ def p_fator_array(p):
     aux += f"pushi {p.parser.variaveis_arrays[p[1].strip()][0]}\n"
     aux += "padd\n"
     aux += f"pushi {p[3]}\n"
+    aux += "loadn\n"
+    p[0] = aux
+
+def p_fator_array_id(p):
+    "fator : ID '[' ID ']' "
+    aux  = "pushgp\n"
+    aux += f"pushi {p.parser.variaveis_arrays[p[1].strip()][0]}\n"
+    aux += "padd\n"
+    aux += f"pushg {p.parser.variaveis_int[p[3].strip()]}\n"
     aux += "loadn\n"
     p[0] = aux
 
@@ -202,6 +476,14 @@ def p_expressao_relacional_expressao_eq(p):
     aux = f"{p[1]}\n"
     aux += f"{p[3]}\n"
     aux += "equal\n"
+    p[0] = aux
+
+def p_expressao_relacional_expressao_dif(p):
+    "expressao_relacional : expressao DIF expressao"
+    aux = f"{p[1]}\n"
+    aux += f"{p[3]}\n"
+    aux += "equal\n"
+    aux += "not\n"
     p[0] = aux
 
 def p_expressao_logica_not(p):
@@ -260,9 +542,20 @@ def p_imprime_var(p):
 def p_imprime_array(p):
     "operacao : PRINT ID '[' INT ']' ';' "
     aux  = "pushgp\n"
-    aux += f"pushi {p.parser.variaveis_arrays[p[2]][0]}\n"
+    aux += f"pushi {p.parser.variaveis_arrays[p[2].strip()][0]}\n"
     aux += "padd\n"
     aux += f"pushi {p[4]}\n"
+    aux += "loadn\nwritei\n"
+    aux += "pushs \"\\n\"\n"
+    aux += "writes\n" 
+    p[0] = aux
+
+def p_imprime_array_id(p):
+    "operacao : PRINT ID '[' ID ']' ';' "
+    aux  = "pushgp\n"
+    aux += f"pushi {p.parser.variaveis_arrays[p[2].strip()][0]}\n"
+    aux += "padd\n"
+    aux += f"pushg {p.parser.variaveis_int[p[4].strip()]}\n"
     aux += "loadn\nwritei\n"
     aux += "pushs \"\\n\"\n"
     aux += "writes\n" 
@@ -279,6 +572,48 @@ def p_imprime_array2d(p):
     aux += "writes\n" 
     p[0] = aux
 
+def p_imprime_string(p):
+    "operacao : PRINT STRING ';' "
+    p[0] = f"pushs {p[2]}\nwrites\npushs \"\\n\"\nwrites\n"
+
+
+def p_operacao_if(p):
+    "operacao : IF '(' expressao_logica ')' '{' operacoes '}' "
+    aux = p[3]
+    aux += f"jz fimIf{p.parser.if_count}\n"
+    aux += p[6]
+    aux += f"fimIf{p.parser.if_count}:\n"
+    p.parser.if_count += 1
+    p[0] = aux
+
+
+def p_operacao_if_else(p):
+    "operacao : IF '(' expressao_logica ')' '{' operacoes '}' ELSE '{' operacoes '}' "
+    aux = p[3]
+    aux += f"jz elseIf{p.parser.if_count}\n"
+    aux += p[6]
+    aux += f"jump fimIf{p.parser.if_count}\n"
+    aux += f"elseIf{p.parser.if_count}:\n"
+    aux += p[10]
+    aux += f"fimIf{p.parser.if_count}:\n"
+    p.parser.if_count += 1
+    p[0] = aux
+
+def p_operacao_for(p):
+    "operacao : FOR '(' ID EQUAL expressao  ';'  expressao_logica   ';' operacao_inc ')' '{' operacoes '}' "
+    if p[3].strip() in parser.variaveis_int:
+        aux = p[5]
+        aux += f"storeg {parser.variaveis_int[p[3].strip()]}\n"
+        aux += f"cicloFor{p.parser.for_count}:\n"
+        aux += p[7]
+        aux += f"jz cicloForEnd{p.parser.for_count}\n"
+        aux += p[12]
+        aux += p[9]
+        aux += f"jump cicloFor{p.parser.for_count}\n"       
+        aux += f"cicloForEnd{p.parser.for_count}:\n"
+        p.parser.for_count+=1
+        p[0] = aux
+
 
 def p_error(p):
     print("Syntax error! ",p)
@@ -292,11 +627,12 @@ def atribui_float(valor):
 
 parser = yacc.yacc()
 parser.variaveis_int = {}
-parser.variaveis_float = {}
 parser.variaveis_arrays = {}
 parser.variaveis_arrays2d = {}
 parser.variaveis_string = {}
 parser.variaveis = 0
+parser.if_count = 1
+parser.for_count = 1
 result = ""
 
 
@@ -305,10 +641,11 @@ with open(sys.argv[1],"r") as f:
     result += parser.parse(f.read())
 
 if parser.success:
-    print(f"//{parser.variaveis_int}")
-    print(f"//{parser.variaveis_float}")
-    print(f"//{parser.variaveis_arrays}")
-    print(f"//{parser.variaveis_arrays2d}")
-    print(f"//{parser.variaveis_string}")
-    print(result)
+    s = re.split(r"\.",sys.argv[1])
+    if s:
+        with open(f"{s[0]}.vm","w+") as f:
+            f.write(result)
+    else:
+        with open(f"{sys.argv[1]}.vm","w+") as f:
+            f.write(result)
     
